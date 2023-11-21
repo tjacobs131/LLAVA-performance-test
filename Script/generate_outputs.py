f40031e641b206ee1b9299c057578e832a1dc0e0
Tom Jacobs <tomjacobs2003@hotmail.com>
2023-11-21 10:47:00 +0100

    Cleanup and prompt improvement

--- Script/generate_outputs.py
+++ Script/generate_outputs.py
@@ -2,7 +2,9 @@ import replicate
 import os
 import glob
 
-# --- Setup
+
+# --- Setup ----
+
 
 # Get api key from file
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -12,24 +14,29 @@ with open(key_file_path, "r") as key_file:
 
 os.environ["REPLICATE_API_TOKEN"] = str(api_key)
 
-# --- Get data
 
-prompts = ["You are an expert in plant disease detection on leaves, are there any diseases that can you detect in this image?, your options are: rust (mild), rust (extreme), healthy. If you are not sure about the disease, you can say that you are unsure, but you must give your best answer at the end anyway."]
+# --- Get data ---
 
-# Get image paths
+
+prompts = ["You are an expert in plant disease detection on leaves, are there any diseases that can you detect in this image?, your options are: rust (mild), rust (extreme), healthy. You are a part of an automatic farming control loop, therefore a final answer is absolutely necessary. Your job is to reason about the health status about the leaf that is provided. Output your final answer exactly like this: Final answer: [your choice out of the given options]. It does not matter how sure you are about your answer, it is crucial that you make your best guess anyway."]
+
+# Get images
 image_dir = os.path.abspath(os.path.join(script_dir, "..", "ImageData", "img"))
-print(f"Image directory: {image_dir}")  # Debug line
-
 image_paths = glob.glob(os.path.join(image_dir, "**", "*.jpeg"), recursive=True)
 
 print("Amount of requests: " + str(image_dir.__len__() * prompts.__len__()))
 
-# --- Get outputs from the lvlm
+
+# --- Get outputs from the LVLM ---
+
+outputs = []
+image_count = 0
 
 for prompt in prompts:
     print("--- Prompt: " + prompt + " ---")
     for image_path in image_paths:
-        print("\nImage: " + image_path)
+        print("\n\nImage #" + str(image_count) +": "  + image_path)
+        outputs.append("")
         with open(image_path, "rb") as image_file:
             output = replicate.run(
                 "yorickvp/llava-13b:2facb4a474a0462c15041b78b1ad70952ea46b5ec6ad29583c0b29dbd4249591",
@@ -38,4 +45,11 @@ for prompt in prompts:
             )
 
             for item in output:
+                outputs[image_count] += item
                 print(item, end="")
+
+            image_count += 1    
+
+for output in outputs:
+    print(output + "\n")
+
